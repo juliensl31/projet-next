1acec62eded213d8aea1ad16f41d35e9725b6d5f
juliensl31 <47530405+juliensl31@users.noreply.github.com>
2023-05-31 18:29:36 +0200

    commit 404

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '5rem',
+          marginTop: '200px',
+          marginBottom: '10px',
+          color: '#ee6c4d',
+        }}
+      >
+        404
+      </h1>
+      <p style={{ fontSize: '1.5rem' }}>
+        Cette page n existe pas!!!
+      </p>
+    </div>
+  );
+}
